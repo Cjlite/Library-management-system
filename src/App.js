e28db4ec76de66dfcd152ea4a7de799a08f3afc1
chetan <jadhavchetan2506@gmail.com>
2023-09-27 15:15:34 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js'
+import "react-toastify/dist/ReactToastify.css";
+import { Route, Routes } from 'react-router-dom'
+import Login from './Components/Login';
+import Dashboard from './Components/Dashboard';
+import Sidebar from './Components/Sidebar';
+import AddBook from './Components/AddBook';
+import AddStudent from './Components/AddStudent';
+import IssueBook from './Components/IssueBook';
+import ReturnBook from './Components/ReturnBook';
+import AddUser from './Components/AddUser';
+
+function App() {
+  return (
+    <>
+      <Routes>
+        {/* <Route path='/' element={<Login />} /> */}
+        <Route path='/' element={<Sidebar />}>
+          <Route path='dashboard' element={<Dashboard />} />
+          <Route path='addbook' element={<AddBook />} />
+          <Route path='addstudent' element={<AddStudent />} />
+          <Route path='issuebook' element={<IssueBook />} />
+          <Route path='returnbook' element={<ReturnBook />} />
+          <Route path='adduser' element={<AddUser />} />
+        </Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,106 @@
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import "../Components/NavBar.css";
+
+const Navbar = () => {
+    const [resourcesOpen, setResourcesOpen] = useState(false);
+    const [lib, setlib] = useState(false);
+    const [program, setprogram] = useState(false);
+
+    const programToggle = () => {
+        setprogram(!program);
+    };
+
+    const LibToggle = () => {
+        setlib(!lib);
+    };
+
+    const resourceshandleToggle = () => {
+        setResourcesOpen(!resourcesOpen);
+    };
+
+    const navigate = useNavigate();
+    const [isHovered, setIsHovered] = useState(false);
+
+    const handleMouseEnter = () => {
+        setIsHovered(true);
+    };
+
+    const handleMouseLeave = () => {
+        setIsHovered(false);
+    };
+
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-primary position-fixed-top">
+                <div className="container">
+                    <a className="navbar-brand" href="#">   </a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div className="navbar-nav gap-1">
+                            <Link to="/" className="nav-link m-1" >Home</Link>
+                            <Link to="dashboard" className="nav-link m-1" >Dashboard</Link>
+                            <Link to="/lib" className="a dropdown nav-link" onMouseEnter={e => setlib(true)} onMouseLeave={e => setlib(false)}>
+                                <div
+                                    className="dropdown-toggle custom-dropdown-toggle"
+                                    onClick={LibToggle}
+                                >
+                                    Library
+                                </div>
+                                <ul className={`dropdown-menu  ${lib ? 'show' : ''}`}>
+                                    <li><a className="dropdown-item" href="#">Library</a></li>
+                                    <li><a className="dropdown-item" href="#">Solution Overviews</a></li>
+                                    <li><a className="dropdown-item" href="#">Videos</a></li>
+                                    <li><a className="dropdown-item" href="#">Blogs</a></li>
+                                    <li><a className="dropdown-item" href="#">Case Studies</a></li>
+                                    <li><a className="dropdown-item" href="#">Webinars</a></li>
+                                    <li><a className="dropdown-item" href="#">OpenRAN</a></li>
+                                </ul>
+                            </Link>
+
+                            <Link to="/stepper" className="a dropdown nav-link" onMouseEnter={e => setResourcesOpen(true)} onMouseLeave={e => setResourcesOpen(false)}>
+                                <div
+                                    className="dropdown-toggle custom-dropdown-toggle"
+                                    onClick={resourceshandleToggle}
+                                >
+                                    Admission
+                                </div>
+                                <ul className={`dropdown-menu  ${resourcesOpen ? 'show' : ''}`}>
+                                    <li><a className="dropdown-item" href="#">Admission-Form</a></li>
+                                    <li><a className="dropdown-item" href="#">Fee-Form</a></li>
+                                    <li><a className="dropdown-item" href="#">Videos</a></li>
+                                    <li><a className="dropdown-item" href="#">Application-Form</a></li>
+                                    <li><a className="dropdown-item" href="#">Case Studies</a></li>
+                                    <li><a className="dropdown-item" href="#">Webinars</a></li>
+                                    <li><a className="dropdown-item" href="#">OpenRAN</a></li>
+                                </ul>
+                            </Link>
+
+                            <Link to="/stepper" className="a dropdown nav-link" onMouseEnter={e => setprogram(true)} onMouseLeave={e => setprogram(false)}>
+                                <div
+                                    className="dropdown-toggle custom-dropdown-toggle"
+                                    onClick={programToggle}
+                                >
+                                    program
+                                </div>
+                                <ul className={`dropdown-menu  ${program ? 'show' : ''}`}>
+                                    <li><a className="dropdown-item" href="#">Exhibition</a></li>
+                                    <li><a className="dropdown-item" href="#">Gathering</a></li>
+                                    <li><a className="dropdown-item" href="#">Teacher-Day</a></li>
+                                    <li><a className="dropdown-item" href="#">Program-study</a></li>
+                                    <li><a className="dropdown-item" href="#">Case Studies</a></li>
+                                    <li><a className="dropdown-item" href="#">Webinars</a></li>
+                                    <li><a className="dropdown-item" href="#">OpenRAN</a></li>
+                                </ul>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </>
+    );
+};
+
+export default Navbar;
